d478acc896998ff8ba7f1bc33317004a53fb7ce9
Fred Menge <mengeprofissional@gmail.com>
2024-10-29 15:33:33 -0300

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import prettier from 'eslint-plugin-prettier'
+import configPrettier from 'eslint-config-prettier'
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      semi: 'error',
+      'prefer-const': 'error',
+      // Prettier como regra de erro
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off'
+    }
+  },
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    // Configuração do Prettier para desativar regras conflitantes
+    plugins: { prettier },
+    rules: configPrettier.rules
+  },
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off'
+    }
+  }
+]
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>OLAAAA</h1>
+      <h2>Mundo</h2>
+    </>
+  )
+}
+
+export default App
