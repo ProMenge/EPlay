f4730a595dbf9038747451601d045f55994905c9
Fred Menge <mengeprofissional@gmail.com>
2024-12-18 15:21:57 -0300

    :sparkles: Feat: Finishing the Gallery component, starting the Gallery modal.

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,26 +1,80 @@
 import Section from '../Section'
-import { Item, Items } from './styles'
+import { Item, Items, Action, Modal, ModalContent } from './styles'
+
+import spider from '../../assets/images/Banner.png'
 
 import Hogwarts from '../../assets/images/HogwartsHero.png'
+import Play from '../../assets/images/Play.png'
+import Zoom from '../../assets/images/Zoom.png'
+import Close from '../../assets/images/close.png'
 
-const Gallery = () => {
+type GalleryItem = {
+  type: 'image' | 'video'
+  url: string
+}
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: Hogwarts
+  },
+  {
+    type: 'image',
+    url: spider
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/1O6Qstncpnc?si=a1whlvTHeoaX861E'
+  }
+]
+
+type Props = {
+  defaultCover: string
+  name: string
+}
+
+const Gallery = ({ defaultCover, name }: Props) => {
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  const getMediaIcon = (item: GalleryItem) => {
+    if (item.type === 'image') return Zoom
+    return Play
+  }
+
   return (
-    <Section background="black" title="Gallery">
-      <Items>
-        <Item>
-          <img src={Hogwarts} alt="" />
-        </Item>
-        <Item>
-          <img src={Hogwarts} alt="" />
-        </Item>
-        <Item>
-          <img src={Hogwarts} alt="" />
-        </Item>
-        <Item>
-          <img src={Hogwarts} alt="" />
-        </Item>
-      </Items>
-    </Section>
+    <>
+      <Section background="black" title="Gallery">
+        <Items>
+          {mock.map((media, index) => (
+            <Item key={media.url}>
+              <img
+                src={getMediaCover(media)}
+                alt={`Media ${index + 1} from ${name}`}
+              />
+              <Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt="Click to maximize the media"
+                />
+              </Action>
+            </Item>
+          ))}
+        </Items>
+      </Section>
+      <Modal>
+        <ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img src={Close} alt="Close Icon" />
+          </header>
+          <img src={spider} alt="" />
+        </ModalContent>
+        <div className="overlay"></div>
+      </Modal>
+    </>
   )
 }
 
--- src/components/Gallery/styles.ts
+++ src/components/Gallery/styles.ts
@@ -1,17 +1,88 @@
 import styled from 'styled-components'
 import { colors } from '../../styles'
 
+export const Action = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.73);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+  transition: opacity 0.5s ease;
+`
+
 export const Items = styled.ul`
   display: flex;
 `
 
 export const Item = styled.li`
   margin-right: 16px;
-  img {
+  position: relative;
+  > img {
     border: 2px solid ${colors.white};
     border-radius: 8px;
     width: 180px;
     height: 180px;
     object-fit: cover;
+  }
+
+  &:hover {
+    ${Action} {
+      opacity: 1;
+      transition: opacity 0.5s ease;
+    }
+  }
+`
+export const Modal = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 1;
+
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.73);
+  }
+`
+export const ModalContent = styled.div`
+  max-width: 960px;
+  position: relative;
+  z-index: 1;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 24px;
+
+    h4 {
+      font-size: 18px;
+      font-weight: bold;
+    }
+
+    img {
+      width: 14px;
+      height: 14px;
+    }
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
   }
 `
